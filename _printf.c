1f61f5f8c38ece0f595217fc0796acea80f8e4d7
root <root@ip-10-42-124-43.ec2.internal>
2026-04-17 14:33:53 +0000

    restructure  _printf with handle_specifier and fix Betty style

--- _printf.c
+++ _printf.c
@@ -1,28 +1,49 @@
 #include "main.h"
 
 /**
+ * handle_specifier - handles conversion specifiers
+ * @format: the format string
+ * @i: current position in format string
+ * @args: va_list of arguments
+ *
+ * Return: number of characters printed
+ */
+int handle_specifier(const char *format, int i, va_list args)
+{
+	count = 0;
+	if (format[i + 1] == 'c')
+		count += print_char(va_arg(args, int));
+	else if (format[i + 1] == 's')
+		count += print_string(va_arg(args, char *));
+	else if (format[i + 1] == '%')
+		count += print_char('%');
+	else if (format[i + 1] == 'd' || format[i + 1] == 'i')
+		count += print_int(va_arg(args, int));
+	return (count);
+}
+
+/**
  * _printf - print output
  * @format: string contain text
  *
  * Return: number of characters printed
  */
 
-int _printf(const char *format , ...)
+int _printf(const char *format, ...)
 {
 	int i;
 	int count;
 	va_list args;
-	
+
 	i = 0;
 	count = 0;
 	va_start(args, format);
-
 	if (format == NULL)
 	{
 		return (-1);
 	}
 
-	while (format != '\0')
+	while (format[i] != '\0')
 	{
 		if (format[i] != '%')
 		{
@@ -33,29 +54,9 @@ int _printf(const char *format , ...)
 
 		else if (format[i] == '%')
 		{
-			if (format[i + 1] == 'c')
-			{
-				count += print_char (va_arg(args, int));
-				i += 2;
-			}	
+			count += handle_specifier(format, i, args);
+			i += 2;
 
-			else if (format[i + 1] == 's')
-			{
-				count += print_string (va_arg(args, char *));
-				i += 2;
-			}
-
-			else if (format[i + 1] == '%')
-			{
-				count += print_char('%');
-				i += 2;
-			}
-
-			else if (format[i + 1] == 'd' || format[i +1] == 'i')
-			{
-				count += print_int (va_arg(args, int));
-				i += 2;
-			}
 		}
 
 	}
--- main.h
+++ main.h
@@ -1,10 +1,10 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
 #include <unistd.h>
 
-int _printf(const char *format , ...);
+int _printf(const char *format, ...);
 int print_int(int n);
 int print_char(char c);
 int print_string(char *str);
